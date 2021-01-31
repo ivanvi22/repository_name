4468d91b2b9af5005d9b5056e043f409e4d00d7b
ivanvi22 <useri@mail.ru>
2021-01-31 21:22:42 +0300

    дописал функции ввода

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,6 +2,9 @@
 car_color = 'wite'
 car_type = 'masda'
 car_count = 2
-
+print("Укажите цвет машины:")
+car_color = input()
+print("Укажите тип машины:")
+car_type = input()
 print("You have " + str(car_count) + ' ' + car_color + ' ' + car_type)
 print(2 ** 3)
